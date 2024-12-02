d7e736753307c76c3dab430896b4efede784f48e
kirin <kirin@safesteps.tech>
2024-12-02 14:53:28 +0000

    fix:styling of kaban board on laptop

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -9,11 +9,10 @@ const SideBar = ({handleLogout}) => {
 
   return (
 <div className={styles.sidebar}> 
-<h2>{user ? user.displayName || user.email : 'Guest'}</h2>
+{/* <h2>{user ? user.displayName || user.email : 'Guest'}</h2> */}
 <ul>
 <li><a href="/dashboard">Dashboard</a></li>
 <li ><a href="/projects">Projects</a></li>
-<li><a href="/calendar">Calendar</a></li>
 
 
 
